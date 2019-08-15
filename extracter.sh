b602b8c07ab9905f08238ea194895d30a25c438b
Sathish Kumar <23581693+Sathishshan@users.noreply.github.com>
2019-08-15 19:36:28 +0530

    Update extracter.sh

--- extracter.sh
+++ extracter.sh
@@ -11,7 +11,7 @@ fi
 
 no_js=$1
 save=$2
-grep -Po '((?:(?<=href="\/url\?q\=)https?:\/\/[\w.-\/%?&:@=\\+#]+))' ${no_js} | sed 's+&amp++g' >> ${save}
+grep -Po '(?:(?<=href="\/url\?q\=)https?:\/\/[\w.-\/%?&:@=\\+#]+)' ${no_js} | sed 's+&amp++g' >> ${save}
 #sed &amp for query string ignorance
 sed -i -r '/^https?:\/\/www\.google\.com/d' ${save}
 #sed google.com is for incase of google preference url
