9bf8190bf073d81dab977875e91f3d6eab4a5387
Sathish Kumar <23581693+Sathishshan@users.noreply.github.com>
2019-08-17 10:23:03 +0530

    Update extracter.sh

--- extracter.sh
+++ extracter.sh
@@ -12,8 +12,8 @@ fi
 no_js=$1
 save=$2
 grep -Po '(?:(?<=href="\/url\?q\=)https?:\/\/[\w.-\/%?&:@=\\+#]+)' ${no_js} | sed 's+&amp++g' >> ${save}
-#sed &amp for query string ignorance
+#sed &amp - For query string ignorance
 sed -i -r '/^https?:\/\/www\.google\.com/d' ${save}
-#sed google.com is for incase of google preference url
+#sed google.com - Is for incase of google preference url
 
 echo "log generated!"
